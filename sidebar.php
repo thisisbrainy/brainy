d87dac4a0a9ec5cae6e0c8026c00436422ebe38e
Asko Nõmm <asko@digitalpress.co>
2016-12-21 00:55:20 +0200

    Fix #4

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,13 @@
+<div class="heading">
+
+	<h2 class="site-title">
+		<a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
+	</h2>
+
+	<p class="site-tagline">
+		<?php bloginfo('description'); ?>
+	</p>
+
+</div>
+
 <?php dynamic_sidebar('sidebar'); ?>
