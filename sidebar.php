16801481f7ee0dd6a023fb1c645c32f088252af2
Asko Nõmm <asko@digitalpress.co>
2016-12-22 06:05:49 +0200

    Fix #15

--- sidebar.php
+++ sidebar.php
@@ -11,3 +11,11 @@
 </div>
 
 <?php dynamic_sidebar('sidebar'); ?>
+
+<div class="footer">
+
+	<p>
+		&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?><br> Designed by <a href="https://asko.me">Asko</a><br> Powered by <a href="https://wordpress.org">WordPress</a>
+	</p>
+
+</div>
