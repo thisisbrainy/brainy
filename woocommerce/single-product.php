ab01cecd743fc52aa41496b5faf446a1e1cd2716
Asko Nõmm <asko@digitalpress.co>
2016-12-22 14:05:25 +0200

    initial WooCommerce support

---
+++ woocommerce/single-product.php
@@ -0,0 +1,35 @@
+<?php
+
+if(!defined('ABSPATH')) exit;
+
+get_header();
+
+?>
+
+<div id="page">
+
+	<div id="sidebar" class="right">
+		<?php get_sidebar(); ?>
+	</div>
+
+	<div id="content" class="left">
+
+		<?php
+
+		do_action('woocommerce_before_main_content');
+
+		while(have_posts()): the_post();
+
+			wc_get_template_part('content', 'single-product');
+
+		endwhile;
+
+		do_action('woocommerce_after_main_content');
+
+		?>
+
+	</div>
+
+</div>
+
+<?php get_footer();
