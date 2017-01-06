cc0e7aa6e8e221beb7d43aa5bbcc9aa5710fdacf
Asko Nõmm <asko@digitalpress.co>
2017-01-06 03:46:09 +0200

    bump

--- functions.php
+++ functions.php
@@ -8,9 +8,6 @@ require_once __DIR__ . '/includes/actions.php';
 require_once __DIR__ . '/includes/functions.php';
 
 /* Assets */
-sidelines_asset('css', 'font-awesome.css');
-sidelines_asset('css', 'reset.css');
-sidelines_asset('css', 'style.css');
-sidelines_asset('js', 'jquery.js');
-//sidelines_asset('js', 'hyphenator.js', true);
-sidelines_asset('js', 'sidelines.js');
+sidelines_asset('css', 'font-awesome.css', false);
+sidelines_asset('css', 'reset.css', false);
+sidelines_asset('css', 'style.css', false);
