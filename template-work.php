5dd8acd9522c1ade5d83c3fb43c0e38fd1c5c665
Asko Nõmm <asko@digitalpress.co>
2017-01-16 07:03:58 +0200

    oops

--- template-work.php
+++ template-work.php
@@ -28,20 +28,6 @@ get_header(); ?>
 
 				</li>
 
-				<li style="background:url(<?php the_post_thumbnail_url(); ?>);">
-
-					<div class="inner">
-
-						<h2><a href="<?php echo explode(':', get_the_title())[1]; ?>"><?php echo explode(':', get_the_title())[0]; ?></a></h2>
-
-						<?php the_content(); ?>
-
-						<small><?php the_time('Y'); ?></small>
-
-					</div>
-
-				</li>
-
 			<?php endwhile; endif; wp_reset_postdata();	?>
 
 		</ul>
